1171c79ebacf6f6f95d5de386379849d02b5ec51
ryo-seed <r.moriya@seed-it.jp>
2024-07-24 20:05:45 +0000

    todoリストのモデル作成

--- app/Model/todo.py
+++ app/Model/todo.py
@@ -1,4 +1,22 @@
-class Todo:
-    def __init__(self, *, description, deadline):
-        self.description = description
-        self.deadline = deadline
+from sqlalchemy import Column, Integer, String, DateTime
+
+# from app.db.db_settings import engine
+from app.db.base_class import Base
+
+
+class Todo(Base):
+    """
+    TODOリストのモデル
+    """
+    
+    __tablename__ = "todos"
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    description = Column(String(255), nullable=False)
+    deadline = Column(DateTime, nullable=True)
+    status = Column(String(10), nullable=False, default="未着手")
+
+
+# if __name__ == "__main__":
+#     Base.metadata.create_all(bind=engine)
+
