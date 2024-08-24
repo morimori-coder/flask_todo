c82f941119ced0203edc502f090f55220db8d19f
Ryo Moriya <ryo.o.3031@gmail.com>
2024-08-24 20:11:07 +0000

    ゴミコードを削除

--- app/Model/todo.py
+++ app/Model/todo.py
@@ -1,22 +1,15 @@
 from sqlalchemy import Column, Integer, String, DateTime
 
 # from app.db.db_settings import engine
-from app.db.base_class import Base
+from app.db.db_settings import Base
 
 
 class Todo(Base):
     """
     TODOリストのモデル
     """
-    
     __tablename__ = "todos"
-    
     id = Column(Integer, primary_key=True, autoincrement=True)
     description = Column(String(255), nullable=False)
     deadline = Column(DateTime, nullable=True)
     status = Column(String(10), nullable=False, default="未着手")
-
-
-# if __name__ == "__main__":
-#     Base.metadata.create_all(bind=engine)
-
