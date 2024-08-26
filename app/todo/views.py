4be78e8aabd53f1da3022c855d4bce12e48b597c
Ryo Moriya <ryo.o.3031@gmail.com>
2024-08-26 20:41:18 +0000

    ゴミコードを削除

--- app/todo/views.py
+++ app/todo/views.py
@@ -10,5 +10,4 @@ def get_todo():
     todo1 = Todo(description="サンプルのTODO1", deadline="2020-12-31")
     todo2 = Todo(description="サンプルのTODO2ああああああ", deadline="2020-12-31")
     todos = [todo1, todo2]
-    print(os.path.join(todo.name, 'todo.html'))
     return render_template("todo.html", todos=todos)
