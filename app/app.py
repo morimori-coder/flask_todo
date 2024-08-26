5afc3979fe57b487f281bf3f306556b11ffc2293
Ryo Moriya <ryo.o.3031@gmail.com>
2024-08-26 20:33:47 +0000

    todoをBlueprintへ分けた

--- app/app.py
+++ app/app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, jsonify
-from Model.todo import Todo
+from todo.views import todo
 
 app = Flask(__name__)
 
+app.register_blueprint(todo)
 
 # 今後の実装方針
 # 1. blueprintを使って、ルーティングを分割する
 # 2. flaskのJinja2テンプレートの継承を使って、共通のレイアウトを作成する
 # 3. データベースを使って、TODOリストを管理する
+
 
 @app.route("/")
 def index():
@@ -19,14 +21,6 @@ def index():
 def hello():
     message = get_hello_message()
     return jsonify({"message": message})
-
-
-@app.route("/todo/", methods=["GET"])
-def get_todo():
-    todo1 = Todo(description="サンプルのTODO1", deadline="2020-12-31")
-    todo2 = Todo(description="サンプルのTODO2ああああああ", deadline="2020-12-31")
-    todos = [todo1, todo2]
-    return render_template("todo.html", todos=todos)
 
 
 def get_hello_message():
---
+++ app/todo/views.py
@@ -0,0 +1,14 @@
+import os
+from flask import Blueprint, render_template
+from Model.todo import Todo
+
+todo = Blueprint("todo", __name__, url_prefix="/todo", template_folder="templates")
+
+
+@todo.route("/", methods=["GET"])
+def get_todo():
+    todo1 = Todo(description="サンプルのTODO1", deadline="2020-12-31")
+    todo2 = Todo(description="サンプルのTODO2ああああああ", deadline="2020-12-31")
+    todos = [todo1, todo2]
+    print(os.path.join(todo.name, 'todo.html'))
+    return render_template("todo.html", todos=todos)
