9e9a7c10e2ade14f03c3bad6ac15b31a92da553c
Ryo Moriya <ryo.o.3031@gmail.com>
2024-09-03 19:58:22 +0000

    todoモデルのカラム定義を変更した

--- app/Model/todo.py
+++ app/Model/todo.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, DateTime, NVARCHAR, Boolean
 
 # from app.db.db_settings import engine
 from app.db.db_settings import Base
@@ -10,6 +10,6 @@ class Todo(Base):
     """
     __tablename__ = "todos"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    description = Column(String(255), nullable=False)
+    description = Column(NVARCHAR(255), nullable=False)
     deadline = Column(DateTime, nullable=True)
-    status = Column(String(10), nullable=False, default="未着手")
+    status = Column(Boolean, default=False)
