15bc4e116267407b05c9f736af3c67fbeec6a31f
Ryo Moriya <ryo.o.3031@gmail.com>
2024-09-08 20:43:07 +0000

    TODOを登録できるようになった

--- app/todo/views.py
+++ app/todo/views.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, request
 from app.Model.todo import Todo
 import logging
+from app.app import db
 
 todo = Blueprint("todo", __name__, url_prefix="/todo",
                  template_folder="templates")
@@ -20,3 +21,14 @@ def get_todo():
         } for todo in todos
     ]
     return render_template("todo.html", todos=todos_list)
+
+
+@todo.route("/add", methods=["POST"])
+def add_todo():
+    description = request.form.get("description")
+    deadline = request.form.get("deadline")
+    status = request.form.get("status")
+    new_todo = Todo(description=description, deadline=deadline, status=status)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect("/todo")
