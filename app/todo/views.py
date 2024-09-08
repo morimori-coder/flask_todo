29f5d24bc0aebab74d76ed08f99f53f7672217fc
Ryo Moriya <ryo.o.3031@gmail.com>
2024-09-08 20:13:44 +0000

    データから取得したTODOを返すよう実装した

--- app/todo/views.py
+++ app/todo/views.py
@@ -1,13 +1,22 @@
 from flask import Blueprint, render_template
 from app.Model.todo import Todo
+import logging
 
 todo = Blueprint("todo", __name__, url_prefix="/todo",
                  template_folder="templates")
 
+logger = logging.getLogger(__name__)
+
 
 @todo.route("/", methods=["GET"])
 def get_todo():
-    todo1 = Todo(description="サンプルのTODO1", deadline="2020-12-31")
-    todo2 = Todo(description="サンプルのTODO2ああああああ", deadline="2020-12-31")
-    todos = [todo1, todo2]
-    return render_template("todo.html", todos=todos)
+    todos = Todo.query.all()
+    todos_list = [
+        {
+            "id": todo.id,
+            "description": todo.description,
+            "deadline": todo.deadline,
+            "status": todo.status
+        } for todo in todos
+    ]
+    return render_template("todo.html", todos=todos_list)
