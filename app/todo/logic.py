ded8ea7ad29fcc58692367a249fedae3e0537ffe
Ryo Moriya <ryo.o.3031@gmail.com>
2024-09-25 20:09:25 +0000

    /addリクエストのチェック関数に説明の長さが0の場合はエラーを返すように処理を追加した

--- app/todo/logic.py
+++ app/todo/logic.py
@@ -1,4 +1,6 @@
 def check_description_length(description):
     if len(description) > 30:
         raise ValueError("Description should be within 30 characters.")
+    if len(description) == 0:
+        raise ValueError("Description should not be empty.")
     return True
